5ac59505a22d11ebc6be684495c1c0b0a14ae73e
Thomas Kite <thomas.kite@student.manchester.ac.uk>
2020-01-05 15:17:06 +0000

    Refactored 602.py according to Dan's advice

--- 602.py
+++ 602.py
@@ -1,89 +1,52 @@
 """
 Advent of Code 2019 - Day 6
 
-This code will read in some puzzle input and find the total number of
-direct and indirect orbits
+This code will read in some puzzle input and find the minimal path between
+YOU and SAN
 
 Author: Tom Kite
 """
 
-from numpy import genfromtxt
+import sys
 
 file_name = "601.dat"
 orbits = {}
-inverse_orbits = {}
 
 
-class Path:
-    bodies = []
-    orbits = {}
-    inverse_orbits = {}
+def read_file(file_name):
+    try:
+        open_file = open(file_name, 'r')
+    except IOError:
+        print("Couldn't open file: %s" % file_name)
+        sys.exit()
 
-    def __init__(self, starting_list, input_orbits, input_inverse_orbits):
-        self.bodies = starting_list
-        self.orbits = input_orbits
-        self.inverse_orbits = input_inverse_orbits
-
-    def see_possibilities(self):
-        current_body = self.bodies[-1]
-        output = []
-        if current_body in self.orbits:
-            for body in self.orbits[current_body]:
-                output.append(body)
-        if current_body in self.inverse_orbits:
-            for body in self.inverse_orbits[current_body]:
-                output.append(body)
-        return output
-
-    def take_all_steps(self):
-        path_list = []
-        for move in self.see_possibilities():
-            new_path = Path(self.bodies+[move], self.orbits, self.inverse_orbits)
-            if (not new_path.does_path_repeat()):
-                path_list.append(new_path)
-        return path_list
-
-    def does_path_repeat(self):
-        for body in self.bodies:
-            if (self.bodies.count(body) > 1):
-                return True
-        return False
-
-def count_orbits_for_body(body_name, orbits):
-    total_orbits = 0
-    while (body_name != "COM"):
-        body_name = orbits[body_name]
-        total_orbits += 1
-    return total_orbits
+    input_data = [x.rstrip("\n") for x in open_file]
+    return input_data
 
 
-input_data = genfromtxt(file_name, dtype=str, delimiter="\n")
+def get_path_to(body, orbits):
+    path = [body]
+    while (path[-1] != "COM"):
+        path.append(orbits[path[-1]])
+    return path
 
-for line in input_data:
-    index = line.find(")")
-    bodyA = line[:index]
-    bodyB = line[index+1:]
 
-    orbits[bodyB] = bodyA
-    inverse_orbits[bodyA] = bodyB
+if __name__ == "__main__":
 
-san_orbit = orbits["SAN"]
-san_path = [san_orbit]
-while (san_path[-1] != "COM"):
-    san_path.append(orbits[san_path[-1]])
+    input_data = read_file(file_name)
 
-my_orbit = orbits["YOU"]
-my_path = [my_orbit]
-while (my_path[-1] != "COM"):
-    my_path.append(orbits[my_path[-1]])
+    for line in input_data:
+        bodyA, bodyB = line.split(")")
+        orbits[bodyB] = bodyA
 
-# Find coincidences
-distances = []
-for my_obj in my_path:
-    if (san_path.count(my_obj) != 0):
-        my_dist = my_path.index(my_obj)
-        san_dist = san_path.index(my_obj)
-        distances.append(my_dist + san_dist)
+    san_path = get_path_to(orbits["SAN"], orbits)
+    my_path = get_path_to(orbits["YOU"], orbits)
 
-print(distances)
-print(min(distances))
+    for body in my_path:
+        if (body in san_path):
+            last_coincidence = body
+            break
+
+    my_dist = my_path.index(last_coincidence)
+    san_dist = san_path.index(last_coincidence)
+    print(my_dist + san_dist)
