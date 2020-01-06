2147e2e9729a7853770732996ad8228edf972ddc
Thomas Kite <thomas.kite@student.manchester.ac.uk>
2020-01-06 11:19:30 +0000

    Refactored day 5 for future intcode assignments

--- 502.py
+++ 502.py
@@ -6,248 +6,133 @@ Introduction
 Author: Tom Kite
 """
 
-from numpy import genfromtxt
-import sys
-
 file_name = "501.dat"
 
-"""
-class int_code:
+
+class int_code():
     code = []
     success = None
-    ID = 0
-
-    def __init__(self, input_code=99, input_ID=1):
-        self.code = input_code
-        self.ID = input_ID
-
-    def _add(self, entry1, entry2, entry3):
-        self.code[entry3] = self.code[entry1] + self.code[entry2]
-        return
-
-    def _mult(self, entry1, entry2, entry3):
-        self.code[entry3] = self.code[entry1] * self.code[entry2]
-        return
-
-    def _input(self, entry):
-        self.code[entry] = self.ID
-        return
-
-    def _output(self, entry):
-        self.code[0] = self.code[entry]
-        return
-
-    def run(self):
-        index = 0
-        while(0 <= index < len(self.code)):
-            if self.code[index] == 1:
-                self._add(self.code[index+1],
-                          self.code[index+2],
-                          self.code[index+3])
-                index += 4
-            elif self.code[index] == 2:
-                self._mult(self.code[index+1],
-                           self.code[index+2],
-                           self.code[index+3])
-                index += 4
-            elif self.code[index] == 3:
-                self._input(self.code[index+1])
-                index += 2
-            elif self.code[index] == 4:
-                self._output(self.code[index+1])
-                return
-            elif self.code[index] == 99:
-                self.success = True
-                return
-            else:
-                self.success = False
-                return
-        self.success = False
-        return
-
-    def show(self):
-        print("Successful run: %s" % self.success)
-        print("Result: %s" % self.code[0])
-        return
-"""
-
-
-def split_op_code(op_code):
-    op_code = str(op_code)
-    run_mode = op_code[3:]
-    op_code = op_code[:len(op_code)-2]
-    if (run_mode == "01" or run_mode == "02"):
-        # Expect 3 modes
-        mode_1 = op_code[-1]
-        mode_2 = op_code[-2]
-        mode_3 = op_code[-3]
-        return run_mode, mode_1, mode_2, mode_3
-    elif (run_mode == "03" or run_mode == "04"):
-        # Expect 1 mode
-        mode_1 = op_code[-1]
-        return run_mode, None, None, mode_1
-    elif (run_mode == "99"):
-        return run_mode, None, None, None
-    else:
-        # Something has gone wrong
-        print("Unable to interpret op code")
-        sys.exit()
-
-
-class int_code:
-    code = []
-    success = None
-    ID = 0
-    run_counter = 0
+    ID = None
     return_value = None
-    index=0
+    index = 0
+    iterations = 0
+    guide = {}
 
-    def __init__(self, input_code=99, input_ID=1):
+    def __init__(self, input_code, input_ID):
         self.code = input_code
         self.ID = input_ID
+        self.guide = {
+                "01": self._code1,
+                "02": self._code2,
+                "03": self._code3,
+                "04": self._code4,
+                "05": self._code5,
+                "06": self._code6,
+                "07": self._code7,
+                "08": self._code8,
+                "99": self._code99,
+                }
 
-    def _add(self, param1, param2, param3, mode="00"):
-        if (mode[0] == "0"): param2 = self.code[param2]
-        if (mode[1] == "0"): param3 = self.code[param3]
-        self.code[param1] = param2 + param3
-        return
-
-    def _mult(self, param1, param2, param3, mode="00"):
-        if (mode[0] == "0"): param2 = self.code[param2]
-        if (mode[1] == "0"): param3 = self.code[param3]
-        self.code[param1] = param2 * param3
-        return
-
-    def _input(self, param):
-        self.code[param] = self.ID
-        return
-
-    def _output(self, param, mode="0"):
-        if (mode == "0"): param = self.code[param]
-        self.return_value = param
-        return
-    
-    def _rule5(self, param1, param2, param3, mode="00"):
-        if (mode[0] == "0"): param2 = self.code[param2]
-        if (mode[1] == "0"): param3 = self.code[param3]
-        if (param2 != "0"):
-            self.code[param1] = param3
-        return
-    
-    def _rule6(self, param1, param2, param3, mode="00"):
-        if (mode[0] == "0"): param2 = self.code[param2]
-        if (mode[1] == "0"): param3 = self.code[param3]
-        if (param2 == "0"):
-            self.code[param1] = param3
-        return
-    
-    def _rule7(self, param1, param2, param3, mode="00"):
-        if (mode[0] == "0"): param2 = self.code[param2]
-        if (mode[1] == "0"): param3 = self.code[param3]
-        if (param2 < param3):
-            self.code[param1] = 1
+    def data_at(self, ind, mode):
+        if (mode == "0"):
+            return self.code[self.code[ind]]
+        elif(mode == "1"):
+            return self.code[ind]
         else:
-            self.code[param1] = 0
+            raise Exception("Invalid mode type %s" % mode)
         return
-    
-    def _rule8(self, param1, param2, param3, mode="00"):
-        if (mode[0] == "0"): param2 = self.code[param2]
-        if (mode[1] == "0"): param3 = self.code[param3]
-        if (param2 == param3):
-            self.code[param1] = 1
+
+    def _code1(self, mode):
+        self.code[self.code[self.index+3]] = \
+                                    self.data_at(self.index+1, mode[0]) +\
+                                    self.data_at(self.index+2, mode[1])
+        self.index += 4
+        return
+
+    def _code2(self, mode):
+        self.code[self.code[self.index+3]] = \
+                                    self.data_at(self.index+1, mode[0]) *\
+                                    self.data_at(self.index+2, mode[1])
+        self.index += 4
+        return
+
+    def _code3(self, mode=None):
+        self.code[self.code[self.index+1]] = self.ID
+        self.index += 2
+        return
+
+    def _code4(self, mode):
+        self.return_value = self.data_at(self.index+1, mode[0])
+        self.index += 2
+        return
+
+    def _code5(self, mode):
+        if (self.data_at(self.index+1, mode[0]) != 0):
+            self.index = self.data_at(self.index+2, mode[1])
+            return
+        self.index += 3
+        return
+
+    def _code6(self, mode):
+        if (self.data_at(self.index+1, mode[0]) == 0):
+            self.index = self.data_at(self.index+2, mode[1])
+            return
+        self.index += 3
+        return
+
+    def _code7(self, mode):
+        if (self.data_at(self.index+1, mode[0]) < self.data_at(self.index+2, mode[1])):
+            self.code[self.code[self.index+3]] = 1
         else:
-            self.code[param1] = 0
+            self.code[self.code[self.index+3]] = 0
+        self.index += 4
+        return
+
+    def _code8(self, mode):
+        if (self.data_at(self.index+1, mode[0]) == self.data_at(self.index+2, mode[1])):
+            self.code[self.code[self.index+3]] = 1
+        else:
+            self.code[self.code[self.index+3]] = 0
+        self.index += 4
+        return
+
+    def _code99(self, mode=None):
+        self.success = True
         return
 
     def run(self):
-        self.index = 0
-        while(0 <= self.index < len(self.code)):
-            self.run_counter += 1
-
-            op_code = str(self.code[self.index])
-            while (len(op_code) != 5):
-                op_code = "0" + op_code
-
-            code_1 = op_code[0]
-            code_3 = op_code[1]
-            code_2 = op_code[2]
-            run_code = op_code[3:]
-
-            if (run_code == "01"):
-                param1 = self.code[self.index+3]
-                param2 = self.code[self.index+1]
-                param3 = self.code[self.index+2]
-                self._add(param1, param2, param3, code_2 + code_3)
-                self.index += 4
-            elif (run_code == "02"):
-                param1 = self.code[self.index+3]
-                param2 = self.code[self.index+1]
-                param3 = self.code[self.index+2]
-                self._mult(param1, param2, param3, code_2 + code_3)
-                self.index += 4
-            elif (run_code == "04"):
-                param = self.code[self.index+1]
-                #if (code_2 == "0"): param = self.code[self.code[index+1]]
-                self._output(param, code_2)
-                self.index += 2
-            elif (run_code == "03"):
-                param = self.code[self.index+1]
-                self._input(param)
-                self.index += 2
-            elif (run_code == "05"):
-                param1 = self.code[self.index+3]
-                param2 = self.code[self.index+1]
-                param3 = self.code[self.index+2]
-                self._rule5(param1, param2, param3, code_2 + code_3)
-                self.index += 4
-            elif (run_code == "06"):
-                param1 = self.code[self.index+3]
-                param2 = self.code[self.index+1]
-                param3 = self.code[self.index+2]
-                self._rule5(param1, param2, param3, code_2 + code_3)
-                self.index += 4
-            elif (run_code == "07"):
-                param1 = self.code[self.index+3]
-                param2 = self.code[self.index+1]
-                param3 = self.code[self.index+2]
-                self._rule5(param1, param2, param3, code_2 + code_3)
-                self.index += 4
-            elif (run_code == "08"):
-                param1 = self.code[self.index+3]
-                param2 = self.code[self.index+1]
-                param3 = self.code[self.index+2]
-                self._rule5(param1, param2, param3, code_2 + code_3)
-                self.index += 4
-            elif (run_code == "99"):
-                print("Exits with code 99")
-                self.success = True
-                return
+        while(self.success is None):
+            self.iterations += 1
+            opcode, mode = get_opcode(self.code[self.index])
+            if opcode in self.guide:
+                self.guide[opcode](mode)
             else:
-                print ("Bad op_code")
                 self.success = False
-                return
-
-        self.success = False
         return
 
     def show(self):
         print("Successful run: %s" % self.success)
+        print("Result: %s" % self.return_value)
         print("Code at 0: %s" % self.code[0])
-        print("Return value: %s" % self.return_value)
-        print("Run consisted of %s steps" % self.run_counter)
+        print("Number of iterations: %s" % self.iterations)
         return
 
-#############
-# MAIN LOOP #
-#############
+
+def get_opcode(input_code):
+    input_code = str(input_code)
+    while (len(input_code) != 4):
+        input_code = "0" + input_code
+    return input_code[2:], input_code[:2][::-1]
 
 
-input_file = [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]#genfromtxt(file_name, dtype=int, delimiter=",")
+def read_data(file_name):
+    with open(file_name) as input_file:
+        output = [int(x) for x in input_file.readline().split(',')]
+    return output
 
-my_int_code = int_code(input_file, 5)
 
-#print(my_int_code.code)
-
-my_int_code.run()
-
-my_int_code.show()
+if __name__ == "__main__":
+    input_file = read_data(file_name)
+    my_int_code = int_code(input_file, 5)
+    my_int_code.run()
+    my_int_code.show()
--- 602.py
+++ 602.py
@@ -7,21 +7,15 @@ YOU and SAN
 Author: Tom Kite
 """
 
-import sys
-
 file_name = "601.dat"
 orbits = {}
+input_data = []
 
 
 def read_file(file_name):
-    try:
-        open_file = open(file_name, 'r')
-    except IOError:
-        print("Couldn't open file: %s" % file_name)
-        sys.exit()
-
-    input_data = [x.rstrip("\n") for x in open_file]
-    return input_data
+    with open(file_name, "r") as input_file:
+        input_data = [x.rstrip("\n") for x in input_file]
+        return input_data
 
 
 def get_path_to(body, orbits):
